535bbe7798eac086c1696542e2e3877c6cb50f66
Osaru Michael <isimichael@hotmail.com>
2023-07-20 11:54:08 +0100

    Modify commit to capture leftover file

---
+++ checkout.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
